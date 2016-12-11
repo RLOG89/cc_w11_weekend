7cc03be8f0d800d2e7f906d1fa9d8676bcbc4b45
RLOG89 <ross.loggie@gmail.com>
2016-12-11 21:50:04 +0000

    added array to replace require

--- public/cities.js
+++ public/cities.js
@@ -1,10 +1,26 @@
-var fs = require('fs');
-var buffer = fs.readFileSync("../data/cities.txt");
-var cities = buffer.toString().split('\n');
+// cannot use due to not being a node file
 
-var cityData = cities.map(function(line) {
-  var cityI = line.split(/\s+/)
-  return console.log(cityI)
-});
+// var fs = require('fs');
+// var buffer = fs.readFileSync("../data/cities.txt");
+// var cities = buffer.toString().split('\n');
 
+// var cityData = cities.map(function(line) {
+//   var cityI = line.split(/\s+/)
+//   return console.log(cityI)
+// });
 
+// var cityData = [
+// {city:"Aberdeen", lat: "57.1497", lon: "-2.0943"},
+// {city:"Edinburgh",lat: "55.9533", lon: "-3.1883"},
+// {city:"Glasgow",  lat: "55.8642", lon: "-4.2518"},
+// {city:"London",   lat: "51.5074", lon: "-0.1278"},
+// ]
+
+var cityData = [
+['Aberdeen', '57.1497', '-2.0943'],
+['Edinburgh', '55.9533', '-3.1883'],
+['Glasgow', '55.8642', '-4.2518'],
+['London', '51.5074', '-0.1278']
+]
+
+ 
