27740c42a3282286822c06eb9314a57a107e4746
RLOG89 <ross.loggie@gmail.com>
2016-12-11 19:55:12 +0000

    added populateCities for populating drop down

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ var app = function() {
   var url = 'https://developers.zomato.com/api/v2.1/search?entity_id=76&entity_type=city&sort=rating&order=desc'
   // var button = document.querySelector('button');
   // button.onclick = handleButtonClick; 
+  populateCities(cityI)
   makeRequest(url, requestComplete);
 }
 
@@ -12,73 +13,75 @@ var makeRequest = function(url, callback) {
   request.setRequestHeader("user-key", key1);
   request.onload = callback; // need to change this later to onclick//
   request.send();
-  };
+};
 
-  var requestComplete = function() {
-    if(this.status !== 200) return;
-    var jsonString = this.responseText;
-    var restaurants = JSON.parse(jsonString);
-    populateList(restaurants)
-  };
+var requestComplete = function() {
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var restaurants = JSON.parse(jsonString);
+  populateCities(cityI)
+  populateList(restaurants)
+};
 
-  var populateList = function(restaurants){
+function populateCities(cityI) {
+  const tag = document.getElementById('city')
+  cityI.forEach(function(city) {
+    const a = createE('select', city[i][0])
+    let aC = appendChild
+    tag.aC(a)
+  })
+}
 
+var populateList = function(restaurants){
+  const getRestaurants = restaurants.restaurants
+  const table = document.getElementById('rContainer')
+  console.log(getRestaurants[0].restaurant)
+  const rName = createE('th', "Restaurant name");
+  const cuisine = createE('th', "Cuisine");
+  const aCost = createE('th', "Average cost for two £");
+  const menu = createE('th', "Menu")
+  table.appendChild(rName);
+  table.appendChild(cuisine);
+  table.appendChild(aCost);
+  table.appendChild(menu);
 
-    const getRestaurants = restaurants.restaurants
-    const table = document.getElementById('rContainer')
-    console.log(getRestaurants[0].restaurant)
-
-    getRestaurants.forEach(function(restaurant){
-      let r = restaurant.restaurant;
-      const tr1 = createE('tr');
-      const td1 = createE('td', "Restaurnt name: " + r.name);
-      const tr2 = createE('tr');
-      const td2 = createE('td', "Cuisine: " + r.cuisines);
-      const tr3 = createE('tr');
-      const td3 = createE('td', "Average cost for two: £" + r.average_cost_for_two);
-      const tr4 = createE('tr');
-      const td4 = createE('a', "Menu:");
-      const tr5 = createE('tr')
-      const img = createE('img');
-      td4.href = r.menu_url;
-      img.src = r.featured_image;
-
-      
-      table.appendChild(tr1)
-      table.appendChild(td1)
-      table.appendChild(tr2)
-      table.appendChild(td2)
-      table.appendChild(tr3)
-      table.appendChild(td3)
-      table.appendChild(tr4)
-      table.appendChild(td4)
-      table.appendChild(tr5)
-      table.appendChild(img)
-
+  getRestaurants.forEach(function(restaurant){
+    let r = restaurant.restaurant;
+    const tr = createE('tr');
+    const td1 = createE('td', r.name);
+    const td2 = createE('td', r.cuisines);
+    const td3 = createE('td', r.average_cost_for_two);
+      // const a = createE('td', r.menu_url);
+      // a.href = r.menu_url;
+      table.appendChild(tr);
+      table.appendChild(td1);
+      table.appendChild(td2);
+      table.appendChild(td3);
+      // table.appendChild(a);
     });
-  };
+};
 
 
-    function createE( tag, innerText ) {
-      let e = document.createElement( tag );
-      if (innerText) {
-        e.innerText = innerText;
-      } 
-      return e;
-  }
+function createE( tag, innerText ) {
+  let e = document.createElement( tag );
+  if (innerText) {
+    e.innerText = innerText;
+  } 
+  return e;
+}
 
-  var handleButtonClick = function() {
+var handleButtonClick = function() {
 
-  }
+}
 
 // use this later to get more results than the initial 20
-  // var increment = function() {
-  //   var counter = 0
-  //   for (var i = 0; i < 100; i++) {
-  //     counter += 20
-  //     }
-  //     return console.log(counter)
-  // }
+function increment() {
+  var counter = 0
+  for (var i = 0; i < 5; i++) {
+    counter ++
+  }
+  return console.log(counter)
+}
 
   // var urlConstructer = function() {
   //   // use filter to find out what user wants to search, pass into params
